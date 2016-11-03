6ccd4d8ade48550148aafa7e19e420b9a6d7ffd2
rhys <rbrooker105@gmail.com>
2016-11-03 17:38:44 +1100

    facebook signup functioning

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,29 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable #omniauthable for facebook signups
          validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false }
+
+
+
+	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+      if user
+       return user
+       else
+       registered_user = User.where(:email => auth.info.email).first
+        if registered_user
+         return registered_user
+         else
+         user = User.create(
+                            provider:auth.provider,
+                            uid:auth.uid,
+                            email:auth.info.email,
+                            password:Devise.friendly_token[0,20],
+                          )
+        end    
+     end
+ end
 end
