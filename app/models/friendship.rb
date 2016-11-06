f0c4ca1ae980e48afdd5a7f3049d35670a8c70f5
rhys <rbrooker105@gmail.com>
2016-11-06 18:16:11 +1100

    friendship model tests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,10 @@
 class Friendship < ApplicationRecord
-	belongs_to :user, :foreign_key => :friend_id
-
   #Friendship table creates and maintains two rows for each friendship
+	belongs_to :user, :foreign_key => :friend_id
+  validates :user_id, presence: true
+  validates :friend_id, presence: true
+
+
   
   #after friendship created, create the inverse friendship in the table
   #and set is_pending to true to define requestee
--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -2,4 +2,55 @@ require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
 
+	def setup
+		@rhys =  users(:rhys)
+		@friend = users(:friend)
+    @friendship = Friendship.create(user_id: @rhys.id,
+                                 friend_id: @friend.id)
+    @inverse_friendship = Friendship.find_by(user_id: @friend.id, 
+    																			 friend_id: @rhys.id)
+
+  end
+
+
+  test "should be valid" do
+    assert @friendship.valid?
+  end
+
+
+  test "should require a friend_id" do
+    @friendship.friend_id = nil
+    assert_not @friendship.valid?
+  end
+
+  test "should require a user_id" do
+    @friendship.user_id = nil
+    assert_not @friendship.valid?
+  end
+
+  test "after creation friendship should be unconfirmed" do
+  	assert @friendship.confirmed == false
+  	assert @inverse_friendship.confirmed == false
+  end
+
+  test "Only the requestee should have a pending friend request" do
+  	assert @friendship.is_pending == false
+  	assert @inverse_friendship.is_pending = true
+  end
+
+  test "After confirming friendship both relations are confirmed" do
+  	@inverse_friendship.confirmed = true
+  	@inverse_friendship.save
+  	@friendship.reload
+  	assert @friendship.confirmed == true
+  	assert @inverse_friendship.confirmed == true
+  end
+
+  test "After destroying friendship both relations are destroyed" do
+  	@friendship.destroy
+  	@inverse_friendship =Friendship.find_by(user_id: @friend.id, 
+    																			 friend_id: @rhys.id)
+  	assert_nil @inverse_friendship
+  end
+
 end
