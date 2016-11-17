f7f30666a01b07ca06d7869ef0a21cec71cca514
Rhys <rbrooker105@gmail.com>
2016-11-17 20:17:45 +1100

    add flash messages to friendship controller actions

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,18 +2,27 @@ class FriendshipsController < ApplicationController
 
   def create
     current_user.request_friendship(params[:id])
+    flash[:success] = "Friend request sent"
     redirect_to :back
   end
 
 
   def update
     current_user.accept_friendship(params[:id])
+    flash[:success] = "Friend request accepted"
     redirect_to :back
+
   end
 
 
   def destroy
-    current_user.destroy_friendship(params[:id])
+    @friendship = Friendship.find_by(user_id: current_user.id, friend_id: params[:id])
+    @friendship.destroy
+    if @friendship.confirmed
+      flash[:success] = "Friendship destroyed"
+    else
+      flash[:success] = "Friendship request denied"
+    end
     redirect_to :back
   end
 
