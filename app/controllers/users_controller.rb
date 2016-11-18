02ad25138b5c0c5dbd8f174fdac97402718fe555
Rhys <rbrooker105@gmail.com>
2016-11-18 13:50:22 +1100

    more posts views to shared folder and render posts on users#show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   
   before_action :find_friend_requests
+  before_action :check_if_friends, only: :show
   
   def index
     @users = current_user.users_list.paginate(page: params[:page]).per_page(10)
   #current_user.users_list
   end
+
+
+  def show
+    @posts = @user.posts.paginate(page: params[:page]).per_page(10)
+  end
+
+  private
+
+    def check_if_friends
+      @user = User.find(params[:id])
+      unless current_user.friends?(@user)
+        flash[:error] = "Only friends of #{@user.first_name} can view this page"
+        redirect_to :root
+      end
+    end
 
 end
